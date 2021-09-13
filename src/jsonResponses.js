db1926914b4d143cdb2de6ee0ca2d11aabf9215e
JohnS-RIT <54949023+JohnS-RIT@users.noreply.github.com>
2021-09-13 13:38:32 -0400

    Update jsonResponses.js

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -2,11 +2,22 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
