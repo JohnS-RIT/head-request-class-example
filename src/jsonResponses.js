c78b0960cd8719be752f600d33e3dbe3a8bdf79d
JohnS-RIT <54949023+JohnS-RIT@users.noreply.github.com>
2021-09-13 13:49:18 -0400

    Update jsonResponses.js

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -21,23 +21,37 @@ const respondJSONMeta = (request, response, status) => {
 };
 
 const getUsers = (request, response) => {
-
+  const responseJSON = {
+    users,
+  };
+  
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const getUsersMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 200);
 };
 
 const updateUser = (request, response) => {
-
+  const newUser = {
+    createdAt: Date.now(),
+  };
+  
+  users[newUser.createdAt] = newUser;
+  return respondJSON(request, response, 201, newUser);
 };
 
 const notFound = (request, response) => {
-
+  const responseJSON = {
+    message: 'The resource you are looking for was not found',
+    id: 'notFound',
+  };
+  
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 const notFoundMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 404);
 };
 
 module.exports = {
